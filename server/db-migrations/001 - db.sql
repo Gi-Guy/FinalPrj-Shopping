e78ab854a5cfef68fd65b874f35c417bb3e8dc25
Guy <Guy9502@Gmail.com>
2025-07-19 12:56:11 +0300

    fixing sql table

--- server/db-migrations/001 - db.sql
+++ server/db-migrations/001 - db.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS users (
   location TEXT,
   bio TEXT,
   avatar TEXT
-
+);
 -- shop tale.sql
 -- TENP UNTIL USERS TABLE IS CREATED
 -- CREATE TABLE IF NOT EXISTS shops (
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS categories (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   description TEXT DEFAULT '',
-  slug TEXT NOT NUL1L,
+  slug TEXT NOT NULL,
   shop_id INTEGER NOT NULL REFERENCES shops(id) ON DELETE CASCADE,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   UNIQUE (slug, shop_id)
