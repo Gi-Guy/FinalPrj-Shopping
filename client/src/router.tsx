e5ec06e7eaaef30b7379e11c32cf731a6fc318aa
Dima Shamis <dims90995@gmail.com>
2025-07-19 17:03:48 +0300

    stuff dammit stufff!!

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import type { RouteObject } from 'react-router-dom';
+
+import Layout from './components/Layout';
+import  HomePage  from './pages/HomePage';
+import UserProfile from './components/UserProfile';
 import CreateShopForm from './pages/CreateShopForm';
 import ShopPage from './pages/ShopPage';
 import CreateCategoryPage from './pages/CreateCategoryPage';
 import NotFoundPage from './pages/NotFoundPage';
 
-export const router = createBrowserRouter([
+const routes: RouteObject[] = [
   {
-    path: '/create-shop',
-    element: <CreateShopForm />,
+    path: '/',
+    element: <Layout />, 
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'profile', element: <UserProfile /> },
+      { path: 'create-shop', element: <CreateShopForm /> },
+      { path: 'shops/:slug', element: <ShopPage /> },
+      { path: 'create-category', element: <CreateCategoryPage /> },
+      { path: '*', element: <NotFoundPage /> },
+    ],
   },
-    {
-    path: '/shops/:slug',
-    element: <ShopPage />,
-  },
-    {
-    path: '/create-category',
-    element: <CreateCategoryPage />
-  },
-    {
-    path: '*',
-    element: <NotFoundPage />
-  }
-]);
+];
+
+const router = createBrowserRouter(routes);
 
 export function AppRouter() {
   return <RouterProvider router={router} />;
