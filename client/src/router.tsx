3997ee9479288c5658994a0de6f6dfbd9307ab83
Guy <Guy9502@Gmail.com>
2025-07-19 12:59:35 +0300

    create category page - temp

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import CreateShopForm from './pages/CreateShopForm';
 import ShopPage from './pages/ShopPage';
+import CreateCategoryPage from './pages/CreateCategoryPage';
+
 export const router = createBrowserRouter([
   {
     path: '/create-shop',
@@ -10,6 +12,10 @@ export const router = createBrowserRouter([
     {
     path: '/shops/:slug',
     element: <ShopPage />,
+  },
+    {
+    path: '/create-category',
+    element: <CreateCategoryPage />
   }
 ]);
 
