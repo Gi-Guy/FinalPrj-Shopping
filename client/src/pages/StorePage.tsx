f8db1f8963d66fe611ca11fc57882ad269355888
Dima Shamis <dims90995@gmail.com>
2025-07-26 20:37:17 +0300

    scss update

--- client/src/pages/StorePage.tsx
+++ client/src/pages/StorePage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import '../scss/StorePage.css';
 
 interface Shop {
   id: number;
